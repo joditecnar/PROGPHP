835d69b896c3e84cf146a0c85ed6a11e1878ebd5
jodiensb <42223664+jodiensb@users.noreply.github.com>
2020-05-09 22:05:24 -0500

    Se crea archivo sintaxis.php
    Se realiza codigo de acuerdo con video tutorial sugerido en Splavia por el Docente.

---
+++ 1. sintaxis.php
@@ -0,0 +1,13 @@
+<?php
+
+//print te permite imprimir o mostrar una sola cadena de datos o resultados //
+
+print "<h1>Bienvenidos a observar mi primer codigo en PHP</h1>";
+
+//echo te permite imprimir o mostrar varias cadenas de datos o resultados //
+
+echo "<p> Yo aprendo programacion en Unitecnar </p>", "<h3> con el Ingeniero Henry Guzman y el Ingeniero Dorismel Perez </h3>";
+
+
+
+?>
